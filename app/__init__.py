947d282c5fc9d1dbc31b442c33ab661e622f74d2
Kevon Haughton <haughtonkevon@gmail.com>
2026-03-16 17:21:37 -0500

    Exercise 1: Add UserProfile model and initial migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,26 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .config import Config
-# import flask migrate here
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-# Instantiate Flask-Migrate library here
+migrate = Migrate(app, db)
 
-# Flask-Login login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+from app.models import UserProfile
+
+@login_manager.user_loader
+def load_user(user_id):
+    return UserProfile.query.get(int(user_id))
+
+# allow migrations to see models
+from app import models
 
 from app import views
